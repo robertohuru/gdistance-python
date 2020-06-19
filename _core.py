55ce73962fa3c85bc366dec5485789af401ad91b
rohuru <robertohuru@gmail.com>
2020-06-20 00:01:51 +0200

    Update _core.py

--- _core.py
+++ _core.py
@@ -9,7 +9,7 @@
 def __bootstrap__():
     global __bootstrap__, __loader__, __file__
     import sys, pkg_resources, imp
-    __file__ = pkg_resources.resource_filename(__name__, 'core.cp37-win_amd64.pyd')
+    __file__ = pkg_resources.resource_filename(__name__, 'core')
     __loader__ = None; del __bootstrap__, __loader__
     imp.load_dynamic(__name__,__file__)
 __bootstrap__()
